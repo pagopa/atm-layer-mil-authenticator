e69a1a70f54741680667d9f1e34dfc3afc44033b
USERSAD\evalenti <emanuele.valentini@nttdata.com>
2024-01-19 13:08:16 +0100

    Handle bad request

--- src/main/java/it/gov/pagopa/atmlayer/service/milauthenticator/enums/AppErrorCodeEnum.java
+++ src/main/java/it/gov/pagopa/atmlayer/service/milauthenticator/enums/AppErrorCodeEnum.java
@@ -13,7 +13,8 @@ public enum AppErrorCodeEnum {
     ATML_MIL_AUTH_500("ATML_MIL_AUTH_500", "An unexpected error has occurred, see logs for more info", GENERIC),
     TOKEN_NOT_FOUND("ATML_MIL_AUTH_00001", "Token not found in cache", KEY_TOKEN_NOT_FOUND),
     MIL_UNAVAILABLE("ATML_MIL_AUTH_00002", "MIL not available", MIL_COMMUNICATION),
-    REDIS_UNAVAILABLE("ATML_MIL_AUTH_00003", "Redis not available", REDIS_COMMUNICATION);
+    REDIS_UNAVAILABLE("ATML_MIL_AUTH_00003", "Redis not available", REDIS_COMMUNICATION),
+    MALFORMED_REQUEST("ATML_MIL_AUTH_00004", "Check your request param", INVALID_ARGUMENT);
 
     private final String errorCode;
     private final String errorMessage;
--- src/main/java/it/gov/pagopa/atmlayer/service/milauthenticator/resource/TokenResource.java
+++ src/main/java/it/gov/pagopa/atmlayer/service/milauthenticator/resource/TokenResource.java
@@ -2,6 +2,8 @@ package it.gov.pagopa.atmlayer.service.milauthenticator.resource;
 
 import io.smallrye.common.constraint.NotNull;
 import io.smallrye.mutiny.Uni;
+import it.gov.pagopa.atmlayer.service.milauthenticator.enums.AppErrorCodeEnum;
+import it.gov.pagopa.atmlayer.service.milauthenticator.exception.AtmLayerException;
 import it.gov.pagopa.atmlayer.service.milauthenticator.model.AuthParameters;
 import it.gov.pagopa.atmlayer.service.milauthenticator.model.ErrorResponse;
 import it.gov.pagopa.atmlayer.service.milauthenticator.model.TokenDTO;
@@ -15,10 +17,13 @@ import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 import lombok.extern.slf4j.Slf4j;
 import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.enums.ParameterIn;
 import org.eclipse.microprofile.openapi.annotations.media.Content;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
@@ -35,57 +40,76 @@ public class TokenResource {
     @Produces(MediaType.APPLICATION_JSON)
     @Operation(summary = "Restituisce il token presente nella cache", description = "Esegue la GET nella cache Redis e restituisce il token trovato tramite i valori di input")
     @APIResponse(responseCode = "200", description = "Operazione eseguita con successo. Il processo è terminato.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = TokenDTO.class)))
+    @APIResponse(responseCode = "400", description = "Uno o più valori di input non valorizzati correttamente", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class)))
     @APIResponse(responseCode = "404", description = "Token non trovato nella cache con i valori di input inseriti", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class)))
     @APIResponse(responseCode = "500", description = "Redis non raggiungibile.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class)))
-    public Uni<TokenDTO> getToken(@HeaderParam("AcquirerId") String acquirerId,
-                                  @HeaderParam("Channel") String channel,
-                                  @HeaderParam("TerminalId") String terminalId,
-                                  @HeaderParam("TransactionId") String transactionId) {
+    public Uni<TokenDTO> getToken(@Parameter(in = ParameterIn.HEADER, required = true)@HeaderParam("AcquirerId") String acquirerId,
+                                  @Parameter(in = ParameterIn.HEADER, required = true)@HeaderParam("Channel") String channel,
+                                  @Parameter(in = ParameterIn.HEADER, required = true)@HeaderParam("TerminalId") String terminalId,
+                                  @Parameter(in = ParameterIn.HEADER, required = true)@HeaderParam("TransactionId") String transactionId) {
 
-        return this.tokenService.getToken(
-                        AuthParameters.builder()
-                                .acquirerId(acquirerId)
-                                .channel(channel)
-                                .terminalId(terminalId)
-                                .transactionId(transactionId)
-                                .build());
+        AuthParameters authParameters = new AuthParameters();
+        try {
+            authParameters = AuthParameters.builder()
+                    .acquirerId(acquirerId)
+                    .channel(channel)
+                    .terminalId(terminalId)
+                    .transactionId(transactionId)
+                    .build();
+        } catch (Exception e) {
+            throw new AtmLayerException("Invalid request parameters", Response.Status.BAD_REQUEST, AppErrorCodeEnum.MALFORMED_REQUEST);
+        }
+        return this.tokenService.getToken(authParameters);
     }
 
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     @Operation(summary = "Restituisce il token creato e lo salva nella cache", description = "Chiama il MIL, CREATE token nella cache Redis e restituisce il token creato")
     @APIResponse(responseCode = "200", description = "Operazione eseguita con successo. Il processo è terminato.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = TokenDTO.class)))
+    @APIResponse(responseCode = "400", description = "Uno o più valori di input non valorizzati correttamente", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class)))
     @APIResponse(responseCode = "500", description = "Servizi esterni (cache, generazione token) non raggiungibili.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class)))
-    public Uni<TokenDTO> createToken(@HeaderParam("AcquirerId") String acquirerId,
-                                     @HeaderParam("Channel") String channel,
-                                     @HeaderParam("TerminalId") String terminalId,
+    public Uni<TokenDTO> createToken(@Parameter(in = ParameterIn.HEADER, required = true)@NotNull @HeaderParam("AcquirerId") String acquirerId,
+                                     @Parameter(in = ParameterIn.HEADER, required = true)@HeaderParam("Channel") String channel,
+                                     @Parameter(in = ParameterIn.HEADER, required = true)@HeaderParam("TerminalId") String terminalId,
                                      @HeaderParam("FiscalCode") String fiscalCode,
-                                     @HeaderParam("TransactionId") String transactionId) {
-
-        return this.tokenService.generateToken(AuthParameters.builder()
-                .acquirerId(acquirerId)
-                .channel(channel)
-                .terminalId(terminalId)
-                .fiscalCode(fiscalCode)
-                .transactionId(transactionId)
-                .build());
+                                     @Parameter(in = ParameterIn.HEADER, required = true)@HeaderParam("TransactionId") String transactionId) {
+        AuthParameters authParameters = new AuthParameters();
+        try {
+            authParameters = AuthParameters.builder()
+                    .acquirerId(acquirerId)
+                    .channel(channel)
+                    .terminalId(terminalId)
+                    .fiscalCode(fiscalCode)
+                    .transactionId(transactionId)
+                    .build();
+        } catch (Exception e) {
+            throw new AtmLayerException("Invalid request parameters", Response.Status.BAD_REQUEST, AppErrorCodeEnum.MALFORMED_REQUEST);
+        }
+        return this.tokenService.generateToken(authParameters);
     }
 
 
     @DELETE
     @Operation(summary = "Cancella il token salvato nella cache", description = "Esegue la DELETE nella cache Redis")
     @APIResponse(responseCode = "204", description = "Operazione eseguita con successo. Il processo è terminato.")
+    @APIResponse(responseCode = "400", description = "Uno o più valori di input non valorizzati correttamente", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class)))
     @APIResponse(responseCode = "404", description = "Token non presente in cache.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class)))
     @APIResponse(responseCode = "500", description = "Redis non raggiungibile.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class)))
-    public Uni<Void> deleteToken(@HeaderParam("AcquirerId") String acquirerId,
-                                 @HeaderParam("Channel") String channel,
-                                 @HeaderParam("TerminalId") String terminalId,
-                                 @HeaderParam("TransactionId") String transactionId) {
-        return this.tokenService.deleteToken(AuthParameters.builder()
-                .acquirerId(acquirerId)
-                .channel(channel)
-                .terminalId(terminalId)
-                .transactionId(transactionId)
-                .build());
+    public Uni<Void> deleteToken(@Parameter(in = ParameterIn.HEADER, required = true)@HeaderParam("AcquirerId") String acquirerId,
+                                 @Parameter(in = ParameterIn.HEADER, required = true)@HeaderParam("Channel") String channel,
+                                 @Parameter(in = ParameterIn.HEADER, required = true)@HeaderParam("TerminalId") String terminalId,
+                                 @Parameter(in = ParameterIn.HEADER, required = true)@HeaderParam("TransactionId") String transactionId) {
+        AuthParameters authParameters = new AuthParameters();
+        try {
+            authParameters = AuthParameters.builder()
+                    .acquirerId(acquirerId)
+                    .channel(channel)
+                    .terminalId(terminalId)
+                    .transactionId(transactionId)
+                    .build();
+        } catch (Exception e) {
+            throw new AtmLayerException("Invalid request parameters", Response.Status.BAD_REQUEST, AppErrorCodeEnum.MALFORMED_REQUEST);
+        }
+        return this.tokenService.deleteToken(authParameters);
     }
 }
