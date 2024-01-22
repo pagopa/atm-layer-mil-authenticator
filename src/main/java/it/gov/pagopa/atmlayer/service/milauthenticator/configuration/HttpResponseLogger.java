0f1953c8beae811bd0bb06fea3e84753fca69bd0
LuciaM1 <lucia.mazzocchi@nttdata.com>
2024-01-22 14:22:19 +0100

    added providers to log requests info

---
+++ src/main/java/it/gov/pagopa/atmlayer/service/milauthenticator/configuration/HttpResponseLogger.java
@@ -0,0 +1,39 @@
+package it.gov.pagopa.atmlayer.service.milauthenticator.configuration;
+
+import it.gov.pagopa.atmlayer.service.milauthenticator.model.TokenDTO;
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerResponseContext;
+import jakarta.ws.rs.container.ContainerResponseFilter;
+import jakarta.ws.rs.ext.Provider;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.temporal.Temporal;
+
+@Provider
+@Slf4j
+public class HttpResponseLogger implements ContainerResponseFilter {
+    public void logResponse(ContainerRequestContext requestContext, ContainerResponseContext responseContext) {
+        log.info("====================================response received with transactionId= {}", requestContext.getHeaderString("TransactionId"));
+        log.info("Status code  : {}", responseContext.getStatus());
+        log.info("Status message  : {}", responseContext.getStatusInfo().getReasonPhrase());
+        log.info("Headers      : {}", responseContext.getHeaders());
+        try {
+            TokenDTO responseToken = (TokenDTO) responseContext.getEntity();
+            log.info("Response token: {}", responseToken.getAccessToken());
+        } catch (Exception e) {
+            log.info("Response body: {}", responseContext.getEntityClass());
+        }
+    }
+
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
+        logResponse(requestContext, responseContext);
+        LocalDateTime timestampEnd = LocalDateTime.now();
+        log.info("Request finished at : {}", timestampEnd);
+        long duration = Duration.between((Temporal) requestContext.getProperty("timestampStart"), timestampEnd).toMillis();
+        log.info("Request duration : {}ms", duration);
+    }
+}
