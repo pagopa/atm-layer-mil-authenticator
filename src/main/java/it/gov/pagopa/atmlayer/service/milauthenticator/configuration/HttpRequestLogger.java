c451250097da949f3bf59ee8e18c76c757b04c65
LuciaM1 <lucia.mazzocchi@nttdata.com>
2024-02-14 17:02:37 +0100

    adding request logs

--- src/main/java/it/gov/pagopa/atmlayer/service/milauthenticator/configuration/HttpRequestLogger.java
+++ src/main/java/it/gov/pagopa/atmlayer/service/milauthenticator/configuration/HttpRequestLogger.java
@@ -2,6 +2,8 @@ package it.gov.pagopa.atmlayer.service.milauthenticator.configuration;
 
 import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.container.ContainerRequestFilter;
+import jakarta.ws.rs.core.MultivaluedMap;
+import jakarta.ws.rs.core.UriInfo;
 import jakarta.ws.rs.ext.Provider;
 import lombok.extern.slf4j.Slf4j;
 
@@ -12,6 +14,10 @@ import java.time.LocalDateTime;
 public class HttpRequestLogger implements ContainerRequestFilter {
     public void logRequest(ContainerRequestContext requestContext) {
         log.info("====================================request started with transactionId= {}, URI : {}, Method : {}", requestContext.getHeaderString("TransactionId"), requestContext.getUriInfo().getAbsolutePath(), requestContext.getMethod());
+        UriInfo uriInfo = requestContext.getUriInfo();
+        MultivaluedMap<String, String> queryParams = uriInfo.getQueryParameters();
+        log.info("URI info:");
+        for (String key : queryParams.keySet()) {log.info(key + ": " + queryParams.get(key));}
     }
 
     @Override
