708fec039778783341d7ca3e9485f823009683da
LuciaM1 <lucia.mazzocchi@nttdata.com>
2024-02-15 10:12:49 +0100

    removing redundant logs

--- src/main/java/it/gov/pagopa/atmlayer/service/milauthenticator/configuration/HttpRequestLogger.java
+++ src/main/java/it/gov/pagopa/atmlayer/service/milauthenticator/configuration/HttpRequestLogger.java
@@ -2,8 +2,6 @@ package it.gov.pagopa.atmlayer.service.milauthenticator.configuration;
 
 import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.container.ContainerRequestFilter;
-import jakarta.ws.rs.core.MultivaluedMap;
-import jakarta.ws.rs.core.UriInfo;
 import jakarta.ws.rs.ext.Provider;
 import lombok.extern.slf4j.Slf4j;
 
@@ -14,10 +12,6 @@ import java.time.LocalDateTime;
 public class HttpRequestLogger implements ContainerRequestFilter {
     public void logRequest(ContainerRequestContext requestContext) {
         log.info("====================================request started with transactionId= {}, URI : {}, Method : {}", requestContext.getHeaderString("TransactionId"), requestContext.getUriInfo().getAbsolutePath(), requestContext.getMethod());
-        UriInfo uriInfo = requestContext.getUriInfo();
-        MultivaluedMap<String, String> queryParams = uriInfo.getQueryParameters();
-        log.info("URI info:");
-        for (String key : queryParams.keySet()) {log.info(key + ": " + queryParams.get(key));}
     }
 
     @Override
