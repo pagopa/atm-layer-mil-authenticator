d45326466833120dc87316c44f583091f431b758
EmVal <emanuele.valentini@nttdata.com>
2024-07-23 16:32:11 +0200

    Fix

--- src/main/java/it/gov/pagopa/atmlayer/service/milauthenticator/resource/CognitoResource.java
+++ src/main/java/it/gov/pagopa/atmlayer/service/milauthenticator/resource/CognitoResource.java
@@ -6,14 +6,10 @@ import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
-import software.amazon.awssdk.services.cognitoidentityprovider.model.UserPoolClientType;
 
 @ApplicationScoped
 @Path("/cognito")
-@Produces(MediaType.APPLICATION_JSON)
 public class CognitoResource {
 
     @Inject
