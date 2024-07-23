310652510a04c98c1d37cd9a2dad360cb96b419f
EmVal <emanuele.valentini@nttdata.com>
2024-07-23 15:22:18 +0200

    Fix cognito endpoint

--- src/main/java/it/gov/pagopa/atmlayer/service/milauthenticator/resource/CognitoResource.java
+++ src/main/java/it/gov/pagopa/atmlayer/service/milauthenticator/resource/CognitoResource.java
@@ -2,6 +2,7 @@ package it.gov.pagopa.atmlayer.service.milauthenticator.resource;
 
 import io.smallrye.mutiny.Uni;
 import it.gov.pagopa.atmlayer.service.milauthenticator.service.impl.CognitoService;
+import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
@@ -9,7 +10,7 @@ import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import software.amazon.awssdk.services.cognitoidentityprovider.model.UserPoolClientType;
 
-
+@ApplicationScoped
 @Path("/cognito")
 @Produces(MediaType.APPLICATION_JSON)
 public class CognitoResource {
