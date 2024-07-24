3e926659d69738760d1e12df9451e02a24b0e0c3
LuciaM1 <lucia.mazzocchi@nttdata.com>
2024-07-24 12:42:42 +0200

    adding AwsClientConf

--- src/main/java/it/gov/pagopa/atmlayer/service/milauthenticator/configuration/AwsClientConf.java
+++ src/main/java/it/gov/pagopa/atmlayer/service/milauthenticator/configuration/AwsClientConf.java
@@ -3,6 +3,7 @@ package it.gov.pagopa.atmlayer.service.milauthenticator.configuration;
 import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import software.amazon.awssdk.http.urlconnection.UrlConnectionHttpClient;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.cognitoidentityprovider.CognitoIdentityProviderClient;
 
@@ -18,6 +19,7 @@ public class AwsClientConf {
         public void init() {
             this.cognitoClient = CognitoIdentityProviderClient.builder()
                     .region(Region.of(config.region())) // Imposta la regione
+                    .httpClientBuilder(UrlConnectionHttpClient.builder())
                     .build();
         }
 
