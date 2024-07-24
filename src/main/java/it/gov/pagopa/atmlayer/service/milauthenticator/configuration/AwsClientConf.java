fb6ddf6c002c52b0ce7fc50e5383d3fd0e294345
LuciaM1 <lucia.mazzocchi@nttdata.com>
2024-07-24 16:52:13 +0200

    adding WebIdentityTokenFileCredentials

--- src/main/java/it/gov/pagopa/atmlayer/service/milauthenticator/configuration/AwsClientConf.java
+++ src/main/java/it/gov/pagopa/atmlayer/service/milauthenticator/configuration/AwsClientConf.java
@@ -4,6 +4,7 @@ import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.WebIdentityTokenFileCredentialsProvider;
 import software.amazon.awssdk.http.urlconnection.UrlConnectionHttpClient;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.cognitoidentityprovider.CognitoIdentityProviderClient;
@@ -27,7 +28,7 @@ public class AwsClientConf {
         this.cognitoClient = CognitoIdentityProviderClient.builder()
                 .httpClientBuilder(UrlConnectionHttpClient.builder())
                 .region(Region.EU_SOUTH_1)
-                .credentialsProvider(StaticCredentialsProvider.create(StsCredentials.getClientCredentials()))
+                .credentialsProvider(WebIdentityTokenFileCredentialsProvider.create())
                 .build();
     }
 
