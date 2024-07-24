cd6bd4da79e2f18260369b4e8677fc661470a5a5
EmVal <emanuele.valentini@nttdata.com>
2024-07-24 13:12:55 +0200

    Added log to DefaultCredentialsProvider

--- src/main/java/it/gov/pagopa/atmlayer/service/milauthenticator/configuration/AwsClientConf.java
+++ src/main/java/it/gov/pagopa/atmlayer/service/milauthenticator/configuration/AwsClientConf.java
@@ -3,6 +3,7 @@ package it.gov.pagopa.atmlayer.service.milauthenticator.configuration;
 import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.http.urlconnection.UrlConnectionHttpClient;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.cognitoidentityprovider.CognitoIdentityProviderClient;
@@ -15,13 +16,14 @@ public class AwsClientConf {
         @Inject
         CognitoConfig config;
 
-        @PostConstruct
-        public void init() {
-            this.cognitoClient = CognitoIdentityProviderClient.builder()
-                    .region(Region.of(config.region())) // Imposta la regione
-                    .httpClientBuilder(UrlConnectionHttpClient.builder())
-                    .build();
-        }
+    @PostConstruct
+    void init() {
+        this.cognitoClient = CognitoIdentityProviderClient.builder()
+                .httpClientBuilder(UrlConnectionHttpClient.builder())
+                .region(Region.of(config.region()))
+                .credentialsProvider(DefaultCredentialsProvider.create())
+                .build();
+    }
 
         public CognitoIdentityProviderClient getCognitoClient() {
             return cognitoClient;
